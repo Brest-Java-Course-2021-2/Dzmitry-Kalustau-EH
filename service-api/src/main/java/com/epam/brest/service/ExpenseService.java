2b5af312c9b3af273896bc885309a04cb35d0b13
Dzmitry-Kalustau <jskov259259@rambler.ru>
2021-12-18 16:51:54 +0300

    ServiceAPI documentation

--- service-api/src/main/java/com/epam/brest/service/ExpenseService.java
+++ service-api/src/main/java/com/epam/brest/service/ExpenseService.java
@@ -6,19 +6,55 @@ import com.epam.brest.model.exceptions.IncorrectExpense;
 
 import java.util.List;
 
+/**
+ * Expense Service Interface.
+ */
 public interface ExpenseService {
 
+    /**
+     * Get all expenses
+     * @return Expense list
+     */
     List<Expense> findAllExpenses();
 
+    /**
+     * Get expense by id
+     * @param expenseId - expense id
+     * @return - Expense with given id
+     */
     Expense getExpenseById(Integer expenseId);
 
+    /**
+     * Create new Expense
+     * @param expense - expense to create
+     * @return - id of the created expense
+     * @throws - expense with a nonexistent category
+     */
     Integer create(Expense expense) throws IncorrectExpense;
 
+    /**
+     * Update Expense
+     * @param expense - expense to update
+     * @return - number of updated rows
+     */
     Integer update(Expense expense);
 
+    /**
+     * Delete Expense by id
+     * @param expenseId - expense id to delete
+     * @return - number of deleted rows
+     */
     Integer delete(Integer expenseId);
 
+    /**
+     * Get id of last expense
+     * @return - id of last expense
+     */
     Integer getIdOfLastExpense();
 
+    /**
+     * Get count of all expenses
+     * @return - count of all expenses
+     */
     Integer count();
 }
--- service-api/src/main/java/com/epam/brest/service/dto/CalculateSumDtoService.java
+++ service-api/src/main/java/com/epam/brest/service/dto/CalculateSumDtoService.java
@@ -6,14 +6,36 @@ import com.epam.brest.model.dto.LocalDateContainer;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * CalculateSumDto Service Interface.
+ */
 public interface CalculateSumDtoService {
 
+    /**
+     * Get all categories with total expenses
+     * @return CalculateSumDto list
+     */
     List<CalculateSumDto> findAllWithSumOfExpenses();
 
+    /**
+     * Get categories with total expenses between dates
+     * @param localDateFrom - date from
+     * @param localDateTo- date to
+     * @return - CalculateSumDto list
+     */
     List<CalculateSumDto> findSumOfExpensesBetweenDates(String localDateFrom, String localDateTo);
 
+    /**
+     * Get LocalDate Container with current dates
+     * @return LocalDate Container
+     */
     LocalDateContainer getLocalDateContainer();
 
+    /**
+     * Changes dates in LocalDate container
+     * @param localDateFrom - date from
+     * @param localDateTo - date to
+     */
     void editLocalDateContainer(LocalDate localDateFrom, LocalDate localDateTo);
 
 }
