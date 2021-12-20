a8f1093f4f9a53bf0db11d43470bd7f96b3debbd
Dzmitry-Kalustau <jskov259259@rambler.ru>
2021-12-20 17:43:23 +0300

    clear RestApplication

--- rest-app/src/main/java/com/epam/brest/RestApplication.java
+++ rest-app/src/main/java/com/epam/brest/RestApplication.java
@@ -19,16 +19,4 @@ public class RestApplication extends SpringBootServletInitializer {
         SpringApplication.run(RestApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            logger.debug("Let's inspect the beans provided by Spring Boot:");
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-////                logger.debug(beanName);
-//                System.out.println(beanName);
-//            }
-        };
-    }
 }
