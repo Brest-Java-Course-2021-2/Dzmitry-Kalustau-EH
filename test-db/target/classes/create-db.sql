7654ea6b92b3b8248a3313b0e53bf56f1ea35694
Dzmitry-Kalustau <jskov259259@rambler.ru>
2021-12-21 20:00:32 +0300

    init commit

---
+++ test-db/target/classes/create-db.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS category;
+
+DROP TABLE IF EXISTS expense;
+
+CREATE TABLE category
+(
+  category_id INT NOT NULL AUTO_INCREMENT,
+  category_name VARCHAR(50) UNIQUE,
+  CONSTRAINT category_pk PRIMARY KEY (category_id)
+);
+
+CREATE TABLE expense
+(
+  expense_id INT NOT NULL AUTO_INCREMENT,
+  date DATE NOT NULL,
+  category_id INT NOT NULL,
+  price DECIMAL NOT NULL,
+    CONSTRAINT expense_pk PRIMARY KEY (expense_id),
+    CONSTRAINT expense_category_fk FOREIGN KEY (category_id) REFERENCES category (category_id)
+);
