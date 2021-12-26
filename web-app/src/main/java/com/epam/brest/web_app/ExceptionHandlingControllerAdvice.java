b29b10771776e99276a12cd947707f59dc5348f2
Dzmitry-Kalustau <jskov259259@rambler.ru>
2021-12-26 17:47:13 +0300

    change ExceptionHandler

--- web-app/src/main/java/com/epam/brest/web_app/ExceptionHandlingControllerAdvice.java
+++ web-app/src/main/java/com/epam/brest/web_app/ExceptionHandlingControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.epam.brest.web_app;
 
+import com.epam.brest.model.exceptions.IncorrectExpense;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
@@ -15,18 +16,6 @@ import java.util.Date;
 public class ExceptionHandlingControllerAdvice {
 
     private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlingControllerAdvice.class);
-
-    @ExceptionHandler({SQLException.class, DataAccessException.class})
-    public ModelAndView handleDataIntegrityViolationException(HttpServletRequest req, Exception ex) {
-        logger.error("Request: " + req.getRequestURL() + " raised " + ex);
-
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", ex);
-        mav.addObject("url", req.getRequestURL());
-        mav.addObject("timestamp", new Date());
-        mav.setViewName("error-db");
-        return mav;
-    }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handleError(HttpServletRequest req, Exception ex) {
